da030afb350580bac9d2dae324c7013f332c35e4
a-moseman <drewmoseman@gmail.com>
2024-07-17 11:54:20 -0400

    Add JavaDoc

--- src/main/java/org/amoseman/wavefunctioncollapse/model/EntropyMatrix.java
+++ src/main/java/org/amoseman/wavefunctioncollapse/model/EntropyMatrix.java
@@ -4,10 +4,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Represents a matrix of entropy corresponding to a matrix of fields.
+ */
 public class EntropyMatrix {
     private final double[] entropy;
     private final List<Integer> indices;
 
+    /**
+     * Instantiate a new entropy matrix of the given size.
+     * @param size the size of the matrix.
+     */
     public EntropyMatrix(int size) {
         this.entropy = new double[size];
         this.indices = new ArrayList<>();
@@ -17,6 +24,10 @@ public class EntropyMatrix {
         Collections.shuffle(indices);
     }
 
+    /**
+     * Get the index of the lowest entropy in the matrix.
+     * @return the index.
+     */
     public int getIndexOfLowest() {
         int least = 0;
         for (Integer index : indices) {
@@ -27,6 +38,11 @@ public class EntropyMatrix {
         return least;
     }
 
+    /**
+     * Set the entropy at the provided index.
+     * @param index the index.
+     * @param e the entropy.
+     */
     public void set(int index, double e) {
         entropy[index] = e;
     }
