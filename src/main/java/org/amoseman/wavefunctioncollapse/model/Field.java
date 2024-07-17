974868f50b89273f69849a519268b2b1c501a640
a-moseman <drewmoseman@gmail.com>
2024-07-17 11:52:26 -0400

    Add JavaDoc

--- src/main/java/org/amoseman/wavefunctioncollapse/model/Field.java
+++ src/main/java/org/amoseman/wavefunctioncollapse/model/Field.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Represents a superposition.
+ */
 public class Field {
     private final int states;
     private final List<Integer> superposition;
     private int state;
 
+    /**
+     * Instantiate a new superposition with a given number of possible states.
+     * @param states the number of possible states.
+     */
     public Field(final int states) {
         this.states = states;
         superposition = new ArrayList<>();
@@ -24,6 +31,11 @@ public class Field {
         }
     }
 
+    /**
+     * Calculate the Shannon entropy of the field given a set of weights.
+     * @param wave the set of weights.
+     * @return the entropy.
+     */
     public double entropy(Wave wave) {
         if (superposition.size() < 2) {
             return 9999;
@@ -39,6 +51,9 @@ public class Field {
         return entropy / wave.getSum();
     }
 
+    /**
+     * Force the superposition to collapse down to a single state.
+     */
     public void collapse() {
         while (superposition.size() > 1) {
             superposition.remove(0);
@@ -46,11 +61,19 @@ public class Field {
         handleState();
     }
 
+    /**
+     * Remove all but the provided states from the superposition.
+     * @param possible the states to keep.
+     */
     public void keep(List<Integer> possible) {
         superposition.retainAll(possible);
         handleState();
     }
 
+    /**
+     * Should be invoked when the number of possible states changes.
+     * Ensures the state method will return a state when the superposition has collapsed.
+     */
     private void handleState() {
         if (superposition.size() == 1) {
             state = superposition.get(0);
@@ -60,6 +83,10 @@ public class Field {
         }
     }
 
+    /**
+     * Get the collapsed state, or 0 if it has not collapsed.
+     * @return the state.
+     */
     public int state() {
         return state;
     }
