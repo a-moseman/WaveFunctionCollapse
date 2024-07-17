645262c96ceb21c442531acfff13d30c6adcc014
a-moseman <drewmoseman@gmail.com>
2024-07-17 11:46:13 -0400

    Add unit test for Field

---
+++ src/test/java/org/amoseman/wavefunctioncollapse/model/FieldTest.java
@@ -0,0 +1,27 @@
+package org.amoseman.wavefunctioncollapse.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldTest {
+
+    @Test
+    void test() {
+        for (int fold = 0; fold < 10; fold++) {
+            Field field = new Field(4);
+            List<Integer> possible = new ArrayList<>(List.of(1, 2, 3, 4));
+            Collections.shuffle(possible);
+            while (possible.size() > 1) {
+                possible.remove(0);
+            }
+            int actual = possible.get(0);
+            field.keep(possible);
+            assertEquals(actual, field.state());
+        }
+    }
+}
